c0111eae75f1dbf4209af6eaa1ce53e86a53d05b
Nikhil Bhaskar <nikhil38@gmail.com>
2016-02-20 02:21:14 -0500

    renamed some more vars

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -11,11 +11,11 @@ module.exports = function(app, dir) {
   app.post('/index', function(req, res) {
     // Utility - clean up
     var responseObj = fun.utility.stringToObj(req.body.userPosts);
-    var allPostsObj = fun.utility.getAllPosts(responseObj);
-    allPostsObj['data'] = fun.utility.keepObjsInArrayBasedOnKey(allPostsObj, 'likes');
+    var allPostsArray = fun.utility.getAllPosts(responseObj);
+    allPostsArray ['data'] = fun.utility.keepObjsInArrayBasedOnKey(allPostsArray, 'likes');
 
     // Biggest fans
-    var likerIDsArray = fun.biggestFans.getLikerIDs(allPostsObj);
+    var likerIDsArray = fun.biggestFans.getLikerIDs(allPostsArray);
     var likersCount = fun.biggestFans.getLikersCount(likerIDsArray);
     var biggestFans = fun.biggestFans.sortObjByValue(likersCount);
     biggestFans = fun.biggestFans.transformBiggestFans(biggestFans);
